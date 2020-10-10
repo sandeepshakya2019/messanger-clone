482317bcf38664e07ff4f88c0007fd860b44e4f9
Sandeep Kumar <51978973+sandeepshakya2019@users.noreply.github.com>
2020-10-11 03:30:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAsU0hil-l8_YfHxqL0ze7LTRGtQBs-kRQ",
-    authDomain: "facebook-messanger-clone-be293.firebaseapp.com",
-    databaseURL: "https://facebook-messanger-clone-be293.firebaseio.com",
-    projectId: "facebook-messanger-clone-be293",
-    storageBucket: "facebook-messanger-clone-be293.appspot.com",
-    messagingSenderId: "289098097543",
-    appId: "1:289098097543:web:b1e74c238909a3c9fe6922",
-    measurementId: "G-Y7L447Q5G0"
+    //Copy from the firebase 
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 })
 
 const db = firebaseApp.firestore();
